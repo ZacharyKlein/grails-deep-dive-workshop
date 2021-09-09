805163610297e01670010f113dc0d6f03f1db704
Zachary Klein <zak@silver-chalice.com>
2021-09-09 10:33:31 -0500

    Added SubscriberEntityListenerService

---
+++ grails-app/services/deep/dive/SubscriberEntityListenerService.groovy
@@ -0,0 +1,19 @@
+package deep.dive
+
+import groovy.util.logging.Slf4j
+import org.grails.datastore.mapping.engine.event.PostInsertEvent
+
+@Slf4j
+class SubscriberEntityListenerService {
+
+    @grails.events.annotation.Subscriber
+    void afterInsert(PostInsertEvent event) {
+        if (event.entityAccess.getEntity() instanceof SubscriberEntity) {
+            String email = event.entityAccess.getPropertyValue("email")
+            log.info("Received asynchronous subscription with email: ${email}")
+
+            //TODO: Send welcome email
+        }
+    }
+
+}
