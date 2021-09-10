9ed41b11bdbbe66f9dfcdba0f4efe90370e082df
Zachary Klein <zak@silver-chalice.com>
2021-09-10 10:31:07 -0500

    Ignore unimplemented test

--- src/test/groovy/deep/dive/SubscriberEntitySpec.groovy
+++ src/test/groovy/deep/dive/SubscriberEntitySpec.groovy
@@ -1,6 +1,7 @@
 package deep.dive
 
 import grails.testing.gorm.DomainUnitTest
+import spock.lang.Ignore
 import spock.lang.Specification
 
 class SubscriberEntitySpec extends Specification implements DomainUnitTest<SubscriberEntity> {
@@ -11,6 +12,7 @@ class SubscriberEntitySpec extends Specification implements DomainUnitTest<Subsc
     def cleanup() {
     }
 
+    @Ignore
     void "test something"() {
         expect:"fix me"
             true == false
